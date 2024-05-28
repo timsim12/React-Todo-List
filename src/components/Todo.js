1fb6be21f7abd1d7f4934172e9ae757dbf1a32ef
tkare <timothy12336@gmail.com>
2024-05-28 01:50:53 -0400

    Finished half of the todo list project. Just implemented the "go to school" task.

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+
+export const Todo = () => {
+  return (
+    <div className='Todo'>
+      <p>Go to school</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon ifcon={faTrash} />
+      </div>
+    </div>
+  )
+}
